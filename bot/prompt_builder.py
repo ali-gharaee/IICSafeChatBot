2c287382cbe1edeeeb6770a8164233385f55c787
ali-gharaee <gharaee.a@gmail.com>
2024-09-12 21:31:24 +0200

    Update prompt_builder.py
    this version works properly!

--- bot/prompt_builder.py
+++ bot/prompt_builder.py
@@ -2,14 +2,9 @@ class PromptBuilder:
     prompt_prefix: str = (
         "Is the following user message hate speech, racist, or extremist?\n user message:`"
     )
-    prompt_suffix: str = (
-        "`Answer 'yes' or 'no'. Explain why if 'yes'."
-    )
+    prompt_suffix: str = "`Answer 'yes' or 'no'. Explain why if 'yes'.Try to Identify even the slightest of hate speech."
 
-    def build_prompt_str(
-        self, chat_message
-    ) -> str:
-        # prefix + chunks + chat history (ending with "Assistant:")
+    def build_prompt_str(self, chat_message) -> str:
         prompt = self.prompt_prefix
         prompt += chat_message
         prompt += self.prompt_suffix
